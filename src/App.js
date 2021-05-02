020e0268b24dcedc03def4fc9c811580fd4355c0
Derek Wang <guanghui-wang@foxmail.com>
2021-05-02 11:58:38 +0800

    feat: App.js calls serverless function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
+import { useEffect, useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const fetchData = async () => {
+    const response = await fetch('/.netlify/functions/helloWorld');
+    return await response.json();
+  };
+
+  useEffect(() => {
+    fetchData().then((res) => {
+      setMessage(res.message);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>{message}</p>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
